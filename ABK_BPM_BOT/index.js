dcdd41bc652df9c91c3cf25dc09d17b6e9924c1d
yerke <yerke@10.131.0.36>
2018-05-04 17:59:27 +0600

    2

--- ABK_BPM_BOT/index.js
+++ ABK_BPM_BOT/index.js
@@ -1,3 +1,4 @@
+const format = require('string-format');
 var config = require('nconf');
 config.file('def', {file: './config/default.json'});
 config.file('tkn', {file: './config/token.json'});
@@ -5,48 +6,60 @@ var TOKEN = config.get('token');
 var TelegramBot = require('node-telegram-bot-api');
 
 // Включить опрос сервера
-var bot = new TelegramBot(TOKEN, {polling: true, polling: {params: {timeout : 300}}});
+var bot = new TelegramBot(TOKEN, {polling: true, request : {proxy : config.get('proxy')}, polling: {params: {timeout : 300}}});
+
+//TO-DO check registration
+var isRegistered = true;
 
 // Начать
-bot.onText(/\/start/, function (msg, match) {
-  bot.sendMessage(getChatId(msg), config.get('hello') + ', ' + msg.from.first_name + '!');
+bot.onText(/\/start/, function (msg, match) {	
+	if(isRegistered) {				
+		bot.sendMessage(
+				getChatId(msg),
+				config.get('hello') + ', ' + msg.from.first_name + '!\n' + config.get('main_menu_txt'),
+			    getMenu(config.get('main_menu_buttons'))
+		);				
+	} else {		
+		bot.sendMessage(
+				getChatId(msg), 
+				config.get('hello') + ', <b>' + msg.from.first_name + '</b>!\n' + format(config.get("telegram_id"), msg.from.id), 
+				{parse_mode : "HTML"}
+		);		
+	}	
 });
-
-
-// bot.on('message', (msg) => {
-//   bot.sendMessage(getChatId(msg), 'Other : ' + JSON.stringify(msg));
-// });
-
-// функции
-// function removePreviousMsg(chatId, message_id) {
-//   try {
-//     bot.deleteMessage(chatId, message_id);
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
-
-function getChatId(msg) {
-  return msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
-}
-/*
 
 // Основное меню
 bot.onText(/\/menu/, function (msg, match) {
-  bot.sendMessage(
-    getChatId(msg),
-    config.get('hello') + ', ' + msg.from.first_name + ', ' + config.get('main_menu_txt'),
-    getMenu(config.get('main_menu_buttons'))
-  );
+	if(isRegistered) {		
+		bot.sendMessage(
+				getChatId(msg),
+				config.get('main_menu_txt'),
+			    getMenu(config.get('main_menu_buttons'))
+		);
+	} else {		
+		bot.sendMessage(
+				getChatId(msg), 
+				format(config.get("telegram_id"), msg.from.id), 
+				{parse_mode : "HTML"}
+		);		
+	}
 });
 
-// Помощь
-bot.onText(/\/help/, function (msg, match) {
-  bot.sendMessage(
-    getChatId(msg),
-    config.get('developing'),
-    getMenu(config.get('alfa_site_url_buttons'))
-  );
+//Помощь
+bot.onText(/\/help/, function (msg, match) {  
+  if(isRegistered) {
+	  bot.sendMessage(
+				getChatId(msg),
+				config.get('help_text'),
+			    getMenu(config.get('alfa_site_url_buttons'))
+		);	  
+	} else {		
+		bot.sendMessage(
+				getChatId(msg), 
+				format(config.get("telegram_id"), msg.from.id), 
+				{parse_mode : "HTML"}
+		);		
+	}  
 });
 
 // Обработка событии
@@ -55,7 +68,6 @@ bot.on('callback_query', function (msg) {
 
   switch (msg.data) {
     case 'to_find_process_by_instanse_id':
-      removePreviousMsg(msg);
       bot.sendMessage(
         chatId,
         config.get('main_menu_txt'),
@@ -63,7 +75,6 @@ bot.on('callback_query', function (msg) {
       );
       break;
     case 'to_main_menu':
-      removePreviousMsg(msg);
       bot.sendMessage(
         chatId,
         config.get('main_menu_txt'),
@@ -73,23 +84,20 @@ bot.on('callback_query', function (msg) {
     default:
       bot.answerCallbackQuery(msg.id, 'Вы выбрали: ' + msg.data, false);
   }
-
+  
 });
 
-bot.on('chosen_inline_result', function (msg) {
-  console.log(msg);
-});
+//-----------------
 
-
-
-function getMenu(arr_buttons) {
-  return options = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: arr_buttons,
-      parse_mode: 'Markdown'
-    })
-  };
+function getChatId(msg) {
+  return msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
 }
 
-
-*/
+function getMenu(arr_buttons) {
+	  return options = {
+	    reply_markup: JSON.stringify({
+	      inline_keyboard: arr_buttons,
+	      parse_mode: 'Markdown'
+	    })
+	  };
+}
