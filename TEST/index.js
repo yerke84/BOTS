29b831c91b4cd9446cd376b6b022114ae4d5aa55
Курышбаев Еркебулан <yerke@iMac-Kuryshbayev.local>
2018-07-11 14:04:27 +0600

    cert 2

--- TEST/index.js
+++ TEST/index.js
@@ -1,3 +1,4 @@
+/*
 var fs = require('fs');
 var https = require('https');
 var privateKey  = fs.readFileSync('server.key', 'utf8');
@@ -12,3 +13,33 @@ app.get('/', (req, res) => res.send('Hello World!<br/>' + new Date()))
 var httpsServer = https.createServer(credentials, app);
 
 httpsServer.listen(1984);
+
+*/
+
+var express = require("express");
+
+var https = require("https");
+
+var fs = require("fs");
+
+var app = express();
+
+app.get("/", function(req, res){
+
+  res.send("<h1>helloww<h2>\n");
+
+});
+
+var options = {
+
+key: fs.readFileSync("certs/server.key"),
+
+cert: fs.readFileSync("certs/server.crt")
+
+}
+
+var server = https.createServer(options, app);
+
+console.log("Listen to 3443");
+
+server.listen(3443);
