862b5cea2587199cd574d139270fba5d002b5b08
Курышбаев Еркебулан <yerke@iMac-Kuryshbayev.local>
2018-07-13 13:35:44 +0600

    2

--- TEST/index.js
+++ TEST/index.js
@@ -1,45 +1,38 @@
-/*
 var fs = require('fs');
 var https = require('https');
-var privateKey  = fs.readFileSync('server.key', 'utf8');
-var certificate = fs.readFileSync('server.crt', 'utf8');
+var privateKey  = fs.readFileSync('certs/server.key', 'utf8');
+var certificate = fs.readFileSync('certs/server.crt', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!<br/>' + new Date()))
+app.get('/', (req, res) => res.send('Hello World!<br/>' + new Date()) + '\n')
 
 var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(1984);
+var port = 1984;
+httpsServer.listen(port, function(){
+    console.log(`server started at port ` + port);
+});
+//Ports currently supported for Webhooks: 443, 80, 88, 8443
+//https://tlgrmalfabank.kz:1984
+/*
+var https = require('https');
+var fs = require('fs');
+var express = require('express');
 
-*/
+var options = {
+    key: fs.readFileSync('certs/server.key'),
+    cert: fs.readFileSync('certs/server.key'),
+    requestCert: false,
+    rejectUnauthorized: false
+};
 
-var express = require("express");
-
-var https = require("https");
-
-var fs = require("fs");
 
 var app = express();
 
-app.get("/", function(req, res){
-
-  res.send("<h1>helloww<h2>\n");
-
+var server = https.createServer(options, app).listen(3000, function(){
+    console.log("server started at port 3000");
 });
-
-var options = {
-
-key: fs.readFileSync("certs/server.key"),
-
-cert: fs.readFileSync("certs/server.crt")
-
-}
-
-var server = https.createServer(options, app);
-
-console.log("Listen to 3443");
-
-server.listen(3443);
+*/
